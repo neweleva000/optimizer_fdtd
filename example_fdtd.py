c30d9bac01012360fbf010b2cb5fa938a467d431
Evan <neweleva000@gmail.com>
2025-10-05 21:20:13 -0700

    Reasonable output waves.

--- example_fdtd.py
+++ example_fdtd.py
@@ -7,45 +7,74 @@ from matplotlib import pyplot as plt
 
 fdtd.set_backend("numpy")
 
+#TODO scale wavelength up by e6 and make everything in meters?
+
 #Constants
 dielectric_const = 4
-WAVELENGTH = (3e8) / (180e9 * sqrt(4))
+max_freq = 180e9
+WAVELENGTH = (3e8) / (max_freq * sqrt(4))
 SPEED_LIGHT: float = 299_792_458.0  # [m/s] speed of light
-
 grid_sp = 0.005 * WAVELENGTH
 
+#Grid definition
+outer_dim_x = 500e-6
+outer_dim_y = 500e-6
+dielectric_thickness = 70e-6
 start_vert = 50e-6
-vertical_len = 80e-6 + 2 * start_vert 
+air_size = grid_sp * 5  #Amount of air material between boundaries and conductor in vertical dimension
+conductor_thickness = 0.01 * WAVELENGTH
+vertical_len = 2 * air_size + dielectric_thickness + 2 * conductor_thickness #2 layer stackup
+pml_size = 12
+microstrip_width = 40e-6
 
 grid = fdtd.Grid(
-    shape = (ceil(500e-6/grid_sp), ceil(500e-6/grid_sp), ceil(vertical_len/grid_sp)), # 500um x 500um x 80um (grid_spacing) --> 3D FDTD
+    shape = (ceil(outer_dim_x/grid_sp), ceil(outer_dim_y/grid_sp), ceil(vertical_len/grid_sp)), 
     grid_spacing=grid_sp,
-    permittivity=1,
+    permittivity=1.0,
 )
 
 #Add dielectric
-grid[5:-5,5:-5,start_vert+5e-6: start_vert + 75e-6] = fdtd.Object(permittivity=4, name="diel")
+#grid[5:-5,5:-5,start_vert+5e-6: start_vert + 75e-6] = fdtd.Object(permittivity=dielectric_const, name="dielectric")
+grid[pml_size:-pml_size,\
+        pml_size:-pml_size,\
+        air_size + conductor_thickness: air_size + conductor_thickness + dielectric_thickness]\
+        = fdtd.Object(permittivity=dielectric_const, name="dielectric")
 
 #Add microstrip line 
-grid[230e-6:270e-6, 5:-5, start_vert+75e-6:start_vert +80e-6] = fdtd.AbsorbingObject(permittivity=1, conductivity=5.8e8, name="conductor")
+#grid[230e-6:270e-6, 5:-5, start_vert+75e-6:start_vert +80e-6] = fdtd.AbsorbingObject(permittivity=1, conductivity=5.8e8, name="conductor")
+microstrip_vertical_start = air_size + dielectric_thickness + conductor_thickness 
+grid[outer_dim_x/2 - microstrip_width/2:outer_dim_x/2 + microstrip_width/2,\
+        pml_size:-pml_size,\
+        microstrip_vertical_start:microstrip_vertical_start + conductor_thickness]\
+        = fdtd.AbsorbingObject(permittivity=1, conductivity=5.8e8, name="conductor")
 
 #Add ground plane
-grid[5:-5,5:-5,start_vert: start_vert + 5e-6] = fdtd.AbsorbingObject(permittivity=1, conductivity=5.8e8, name="gnd")
+grid[pml_size:-pml_size,\
+        pml_size:-pml_size,\
+        air_size : air_size + conductor_thickness]\
+        = fdtd.AbsorbingObject(permittivity=1, conductivity=5.8e8, name="gnd")
 
 
 #Add source at far left side (Sx1
-grid[60, 6, start_vert:start_vert+79e-6] = fdtd.LineSource(
-    period = 100e9, name="source"
-)
+grid[outer_dim_x / 2\
+        , pml_size + 1,\
+        air_size:microstrip_vertical_start + conductor_thickness]\
+        = fdtd.LineSource(period = 1/max_freq, name="source")
 
 #Add detector at far right side (Port 1)
-grid[60, 6, start_vert+76e-6:start_vert +79e-6] = fdtd.LineDetector(name="port1")
+grid[outer_dim_x/2,\
+        pml_size +1,\
+        microstrip_vertical_start + conductor_thickness]\
+        = fdtd.LineDetector(name="port1")
 
 #Add detector at far right side (Port 2)
-grid[60, -5, start_vert+76e-6:start_vert +79e-6] = fdtd.LineDetector(name="port2")
+grid[outer_dim_x /2,\
+        -pml_size -1,\
+        microstrip_vertical_start + conductor_thickness]\
+        = fdtd.LineDetector(name="port2")
 
 #Box of size 2
-DomainBorderPML(grid, 2)  
+DomainBorderPML(grid, pml_size)  
 
 grid.run(total_time=1000)
 
