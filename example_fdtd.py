02be6623410bf287e55dcd81d39d11cc9f1cd341
Evan <neweleva000@gmail.com>
2025-10-14 23:00:43 -0700

    Valid results for certain frequency bands.

--- example_fdtd.py
+++ example_fdtd.py
@@ -37,7 +37,8 @@ def get_poynting_freq(E_f_x, E_f_y, E_f_z, H_f_x, H_f_y, H_f_z,):
     S_complex = np.cross(E, H_conj)
     
     # Compute time-averaged (real) Poynting vector: (1/2) Re{E × H*}
-    return 0.5 * np.real(S_complex)
+    #return 0.5 * np.real(S_complex)
+    return 0.5 * np.abs(S_complex)
     
 #Takes time domain E and H field arrays. Returns spectral power.
 #Assuems that fields are sampled evenly across area
@@ -93,24 +94,23 @@ grid_sp = 0.03 * WAVELENGTH
 outer_dim_x_conductor = 500e-6
 outer_dim_y_conductor = 500e-6
 dielectric_thickness = 70e-6
-start_vert = 50e-6
-air_grid_pt = 10
+air_grid_pt = 2 * int(dielectric_thickness / grid_sp)
 air_size = grid_sp * air_grid_pt  #Amount of air material between boundaries and conductor in vertical dimension
 conductor_thickness = grid_sp
 vertical_len = air_size + dielectric_thickness + 2 * conductor_thickness #2 layer stackup
-pml_size = 5
+pml_size = 8
 microstrip_width = 40e-6
 outer_dim_x = outer_dim_x_conductor + 2 * pml_size * grid_sp
 outer_dim_y = outer_dim_y_conductor + 2 * pml_size * grid_sp
 conductor_start = pml_size * grid_sp
-extra_z_pml = 5
+extra_z_pml = 0
 
 #Simulation duration parameters
-min_run_time = 1000 #This is where it appears to level off
-run_time_step = 100
-thresh = 0.005 #Level at which E field must drop to end simulation
+min_run_time = 5000 #This is where it appears to level off
+run_time_step = 500
+thresh = 0.001 #Level at which E field must drop to end simulation
 num_avg = run_time_step
-max_iterations = 10000
+max_iterations = 8000
 max_run_calls = (max_iterations - 1000) // run_time_step
 
 grid = fdtd.Grid(
@@ -120,29 +120,32 @@ grid = fdtd.Grid(
     grid_spacing=grid_sp,
     permittivity=1.0)
 
-#Add air TODO -- is this needed? -- only for conducitivity if so
-grid[pml_size:grid.shape[0]//2 - int(microstrip_width / grid_sp),\
-        pml_size:-pml_size,\
-        vertical_len - air_size +(pml_size-1) * grid_sp:vertical_len +pml_size * grid_sp]\
-        = fdtd.AbsorbingObject(permittivity=1, conductivity=0, name="air1")
+print(grid)
 
-grid[grid.shape[0]//2 + int(microstrip_width / grid_sp):-pml_size,\
-        pml_size:-pml_size,\
-        vertical_len - air_size +(pml_size-1) * grid_sp:vertical_len +pml_size * grid_sp]\
-        = fdtd.AbsorbingObject(permittivity=1, conductivity=0, name="air2")
-
-grid[outer_dim_x/2 - microstrip_width/2:outer_dim_x/2 + microstrip_width/2,\
-        pml_size:-pml_size,\
-        vertical_len - air_size +pml_size * grid_sp:vertical_len +pml_size * grid_sp]\
-        = fdtd.AbsorbingObject(permittivity=1, conductivity=0, name="air3")
+#Add air TODO -- is this needed? -- only for conducitivity if so. Only needed for non zero
+#grid[pml_size:grid.shape[0]//2 - int(microstrip_width / grid_sp),\
+#        pml_size:-pml_size,\
+#        vertical_len - air_size +(pml_size-1) * grid_sp:vertical_len +pml_size * grid_sp]\
+#        = fdtd.AbsorbingObject(permittivity=1, conductivity=0, name="air1")
+#
+#grid[grid.shape[0]//2 + int(microstrip_width / grid_sp):-pml_size,\
+#        pml_size:-pml_size,\
+#        vertical_len - air_size +(pml_size-1) * grid_sp:vertical_len +pml_size * grid_sp]\
+#        = fdtd.AbsorbingObject(permittivity=1, conductivity=0, name="air2")
+#
+#grid[outer_dim_x/2 - microstrip_width/2:outer_dim_x/2 + microstrip_width/2,\
+#        pml_size:-pml_size,\
+#        vertical_len - air_size +pml_size * grid_sp:vertical_len +pml_size * grid_sp]\
+#        = fdtd.AbsorbingObject(permittivity=1, conductivity=0, name="air3")
 
 #Add ground plane
 grid[pml_size:-pml_size,\
         pml_size:-pml_size,\
         conductor_start : conductor_start + conductor_thickness]\
-        = fdtd.AbsorbingObject(permittivity=1, conductivity=5.8e8, name="gnd")
+        = fdtd.AbsorbingObject(permittivity=1, conductivity=1e20, name="gnd")
+        #= fdtd.AbsorbingObject(permittivity=1, conductivity=float('inf'), name="gnd")
 
-#Add dielectric. TODO non zero conductivity
+#Add dielectric. TODO non zero conductivity to model leakage current
 grid[pml_size:-pml_size,\
         pml_size:-pml_size,\
         conductor_start + conductor_thickness: conductor_start + conductor_thickness + dielectric_thickness]\
@@ -159,13 +162,16 @@ grid[outer_dim_x/2 - microstrip_width/2:outer_dim_x/2 + microstrip_width/2,\
 #pulse size = t1 = int(2 * pi / (frequency * hanning_dt / cycle)); where frequency is in step size
 grid[outer_dim_x/2 - microstrip_width/2:outer_dim_x/2 + microstrip_width/2,\
         pml_size,\
-        microstrip_vertical_start:microstrip_vertical_start + conductor_thickness]\
-        = fdtd.LineSource(period = 1/max_freq, name="source", pulse=True, cycle=10, hanning_dt=10.0)
+        #microstrip_vertical_start:microstrip_vertical_start + conductor_thickness]\
+        #microstrip_vertical_start - grid_sp]\
+        microstrip_vertical_start + conductor_thickness: microstrip_vertical_start + conductor_thickness + grid_sp]\
+        = fdtd.LineSource(period = 1/max_freq, name="source", pulse=True, cycle=100, hanning_dt=10.0)
+        #= fdtd.LineSource(period = 1/max_freq, name="source")# pulse=True, cycle=10, hanning_dt=10.0)
 
 
 #Add detector at far left side (Port 1)
 grid[outer_dim_x/2 - microstrip_width/2:outer_dim_x/2 + microstrip_width/2,\
-        pml_size +1,\
+        pml_size,\
         microstrip_vertical_start:microstrip_vertical_start + conductor_thickness]\
         = fdtd.LineDetector(name="port1")
 
@@ -176,7 +182,9 @@ grid[outer_dim_x/2 - microstrip_width/2:outer_dim_x/2 + microstrip_width/2,\
         = fdtd.LineDetector(name="port2")
 
 #Box of size 2
-CustomDomainBorder(grid, pml_size, 1e-8, extra_z_pml)  
+#CustomDomainBorder(grid, pml_size, 1e-8, extra_z_pml)  
+#CustomDomainBorder(grid, pml_size, 1e-1, extra_z_pml)  
+CustomDomainBorder(grid, pml_size, 10, extra_z_pml)  
 
 #Show setup 
 grid.visualize(z=0, show=True)
@@ -247,14 +255,17 @@ plt.plot(freq_array, P1)
 plt.title('P1')
 plt.show()
 
-#mag_S21_sq = (np.sqrt(P2 / P1))
-mag_S21_sq = ((P2 / P1))
+mag_S21_sq = (np.sqrt(P2 / P1))
+#mag_S21_sq = ((P2 / P1))
 plt.plot(freq_array, mag_S21_sq)
-plt.title('S21')
+plt.title('|S21|')
 plt.show()
 
 #Plot E field at port 1
 E_1_t_x = E_1_t[:, 0, 2] #At first spatial step
 plt.plot(range(len(E_1_t_x)), E_1_t_x)
 plt.title('Port 1 E field.')
+plt.show()
+
+plt.plot(np.fft.fftfreq(len(E_1_t_x), grid.time_step), np.abs(np.fft.fft(E_1_t_x)))
 plt.show()
